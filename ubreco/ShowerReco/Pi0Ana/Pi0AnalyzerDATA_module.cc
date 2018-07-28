311c71aff88d62155d6b3252603851279023c748
Usher, Tracy L <usher@slac.stanford.edu>
2018-07-28 14:03:56 -0700

    Removing unused variables which cause compilation crash

--- ubreco/ShowerReco/Pi0Ana/Pi0AnalyzerDATA_module.cc
+++ ubreco/ShowerReco/Pi0Ana/Pi0AnalyzerDATA_module.cc
@@ -72,8 +72,6 @@ private:
   std::vector<int> Match(const std::vector<recob::Shower>& shr_v,
 			 const std::vector<sim::MCShower>& pi0_shower_v);
 			 
-  
-  double _w2cm, _t2cm;
 
   selection::SelectionAlg _pi0selection;
 
@@ -101,29 +99,19 @@ private:
   double _rc_shr_e;
   double _rc_shr_dedx;
   double _rcradlen;
-  // shower-by-shower variables, comparisons
-  double _angle;
-  double _strtdiff;
-  double _dwallmin;
-
+ 
   // pi0 tree information
   double _rc_shr1_x,  _rc_shr1_y,  _rc_shr1_z;
   double _rc_shr1_px, _rc_shr1_py, _rc_shr1_pz;
   double _rc_shr1_e;
   double _rc_shr1_dedx;
   double _rcradlen1;
-  double _angle1;
-  double _strtdiff1;
-  double _dwallmin1;
 
   double _rc_shr2_x,  _rc_shr2_y,  _rc_shr2_z;
   double _rc_shr2_px, _rc_shr2_py, _rc_shr2_pz;
   double _rc_shr2_e;
   double _rc_shr2_dedx;
   double _rcradlen2;
-  double _angle2;
-  double _strtdiff2;
-  double _dwallmin2;
 
   double _rcmass;
   double _rcangle;
--- ubreco/ShowerReco/ShowerReco3D/ModularAlgo/dEdxModule_tool.cc
+++ ubreco/ShowerReco/ShowerReco3D/ModularAlgo/dEdxModule_tool.cc
@@ -129,8 +129,6 @@ namespace showerreco {
     _py = dir3D[1];
     _pz = dir3D[2];
 
-    auto const& geomH = ::util::GeometryUtilities();
-
     // loop through planes
     for (size_t n = 0; n < clusters.size(); n++) {
       
