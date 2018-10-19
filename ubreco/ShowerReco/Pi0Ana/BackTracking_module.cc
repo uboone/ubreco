e874ccde4867ba287a53d6b2c95c7710bb76516f
David Caratelli <devish.c@gmail.com>
2018-10-19 16:23:47 -0500

    completed backtracking module (for hits, not yet for clusters) --DC

--- ubreco/ShowerReco/Pi0Ana/BackTracking_module.cc
+++ ubreco/ShowerReco/Pi0Ana/BackTracking_module.cc
@@ -19,6 +19,10 @@
 
 #include "art/Persistency/Common/PtrMaker.h"
 
+#include "art/Framework/Services/Optional/TFileService.h"
+#include "TTree.h"
+#include "TVector3.h"
+
 //"larsoft" object includes
 #include "lardata/Utilities/AssociationUtil.h"
 #include "lardataobj/MCBase/MCShower.h"
@@ -61,6 +65,21 @@ private:
 
   std::map<size_t, std::vector<unsigned int> > GetMCShowerInfo(const art::ValidHandle<std::vector<simb::MCTruth> > mct_h, const art::ValidHandle<std::vector<sim::MCShower> > mcs_h);
 
+  TTree* _tree;
+  float _shr_etru;
+  float _shr_edep;
+  float _shr_Ehit;
+  float _shr_ehit;
+  float _shr_qhit;
+
+  TTree* _hit_tree;
+  float _ihit;
+  int   _wire;
+  float _ehit;
+  float _Ehit;
+  float _ehitr;
+  float _Ehitr;
+
 };
 
 
@@ -72,7 +91,24 @@ BackTracking::BackTracking(fhicl::ParameterSet const & p)
   
   fHitProducer  = p.get<std::string>("HitProducer");
   fBacktrackTag = p.get<std::string>("BacktrackTag");
+
+  art::ServiceHandle<art::TFileService> tfs;
   
+  _tree = tfs->make<TTree>("_tree","shower completeness ttree");
+  _tree->Branch("_shr_etru",&_shr_etru,"shr_etru/F"); // true energy of the electron
+  _tree->Branch("_shr_edep",&_shr_edep,"shr_edep/F"); // true deposited energy by the shower
+  _tree->Branch("_shr_Ehit",&_shr_Ehit,"shr_Ehit/F"); // truth energy measured from backtracker
+  _tree->Branch("_shr_ehit",&_shr_ehit,"shr_ehit/F"); // truth charge measured from backtracker
+  _tree->Branch("_shr_qhit",&_shr_qhit,"shr_qhit/F"); // reconstructed charge from hits -> MeV via 198. gain and 0.577 recomb.
+
+  _hit_tree = tfs->make<TTree>("_hit_tree","hit ttree");
+  _hit_tree->Branch("_ihit",&_ihit,"ihit/F"); // hit integral [ADCs]
+  _hit_tree->Branch("_ehit",&_ehit,"ehit/F"); // hit electrons, from backtracker
+  _hit_tree->Branch("_Ehit",&_Ehit,"Ehit/F"); // hit energy, from backtracker
+  _hit_tree->Branch("_ehitr",&_ehitr,"ehitr/F"); // hit electrons, from reconstruction
+  _hit_tree->Branch("_Ehitr",&_Ehitr,"Ehitr/F"); // hit energy, from reconstruction
+  _hit_tree->Branch("_wire",&_wire,"wire/I");
+
 }
 
 void BackTracking::analyze(art::Event const & e)
@@ -113,7 +149,12 @@ void BackTracking::analyze(art::Event const & e)
     auto edep = mcs_h->at(shrIDX).DetProfile().E();
     std::cout  << "shower has " << edep << " energy" << std::endl;
 
-    double collectedEnergy = 0.;
+    _shr_edep = edep;
+    _shr_etru = mcs_h->at(shrIDX).Start().E();
+
+    double collectedEnergy   = 0.;
+    double collectedCharge   = 0.;
+    double collectedIntegral = 0.;
 
     std::vector<simb::MCParticle const*> particle_vec;
     std::vector<anab::BackTrackerHitMatchingData const*> match_vec;  
@@ -123,35 +164,60 @@ void BackTracking::analyze(art::Event const & e)
       // get associations for this hit
       auto const hit = hit_h->at(i);
       if (hit.WireID().Plane != 2) continue;
+
+      _ihit = hit.Integral();
+      _wire    = hit.WireID().Wire;
+      _ehit = 0.;
+      _Ehit = 0.;
+
+
       
       // now get the gaushit
       auto const gaushit_idx = hitMap[i];
+      //std::cout << "idx gaushit : " << gaushit_idx << "\t at pt " << i << std::endl;
       //auto const gaushit = gaushit_h->at(gaushit_idx);
       
       particle_vec.clear(); match_vec.clear();
       
       backtrack_h.get(gaushit_idx, particle_vec, match_vec);
       
+      // does this trackID match that of the MCShower?
+      bool matchedID = false;
+
       for(size_t i_p=0; i_p<particle_vec.size(); ++i_p){
 	
 	auto mctrkid = particle_vec.at(i_p)->TrackId();
 
+	_ehit += match_vec[i_p]->numElectrons;
+	_Ehit += match_vec[i_p]->energy;
+
 	// does this trackID match that of the MCShower?
-	bool matchedID = false;
-	for (auto const& shrtrkid : trackid_v) 
+	for (auto const& shrtrkid : trackid_v)  
 	  if ( shrtrkid == (unsigned int)mctrkid ) { matchedID = true; break; }
 	
 	if (matchedID) {
 
-	  //std::cout << "Track ID : " << mctrkid << " has " << match_vec[i_p]->energy << std::endl;
-
 	  collectedEnergy += match_vec[i_p]->energy;
+	  collectedCharge += match_vec[i_p]->numElectrons;
 
 	}// if the mcparticle belongs to the shower
 
       }// for all particles associated to this hit
 
+      if (matchedID) {
+	_ehitr = _ihit * 198;
+	_Ehitr = _ehitr * 23.6 * (1e-6) / 0.577;
+	collectedIntegral += _Ehitr;
+	_hit_tree->Fill();
+      }
+
     }// for all hits
+
+    _shr_Ehit = collectedEnergy;
+    _shr_ehit = collectedCharge;
+    _shr_qhit = collectedIntegral;
+
+    _tree->Fill();
 
     std::cout << " collected energy from hits : " << collectedEnergy << std::endl;
       
