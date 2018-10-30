4a25dc3922e45eb25c3d2616f8bd025b42b7c8e7
David Caratelli <devish.c@gmail.com>
2018-10-29 22:45:04 -0500

    Updating StopMu module to include Truncated Mean calculation --DC

--- ubreco/StopMu/StopMu_module.cc
+++ ubreco/StopMu/StopMu_module.cc
@@ -33,9 +33,234 @@
 #include "TTree.h"
 #include "TVector3.h"
 
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <climits>
+#include <limits>
+
 #include "lardata/DetectorInfoServices/DetectorPropertiesService.h"
 #include "lardata/DetectorInfoServices/DetectorClocksService.h"
 #include "larevt/SpaceChargeServices/SpaceChargeService.h"
+
+
+/**
+   \class TruncMean
+   The truncated mean class allows to compute the following quantities
+   1) the truncated mean profile of an ordered vector of values, such as 
+   the charge profile along a particle's track.
+   To create such a profile use the function CalcTruncMeanProfile()
+   2) Get the truncated mean value of a distribution. This function
+   iteratively hones in on the truncated mean of a distribution by
+   updating the mean and cutting the tails after each iteration.
+   For this functionality use CalcIterativeTruncMean()
+   doxygen documentation!
+*/
+
+static const float kINVALID_FLOAT = std::numeric_limits<float>::max();
+
+class TruncMean;
+
+class TruncMean{
+
+ public:
+
+  /// Default constructor
+  TruncMean(){}
+
+  /// Default destructor
+  ~TruncMean(){}
+
+  /**
+     @brief Given residual range and dq vectors return truncated local dq.
+     Input vectors are assumed to be match pair-wise (nth entry in rr_v
+     corresponds to nth entry in dq_v vector).
+     Input rr_v values are also assumed to be ordered: monotonically increasing
+     or decreasing.
+     For every dq value a truncated linear dq value is calculated as follows:
+     0) all dq values within a rr range set by the class variable _rad are selected.
+     1) the median and rms of these values is calculated.
+     2) the subset of local dq values within the range [median-rms, median+rms] is selected.
+     3) the resulting local truncated dq is the average of this truncated subset.
+     @input std::vector<float> rr_v -> vector of x-axis coordinates (i.e. position for track profile)
+     @input std::vector<float> dq_v -> vector of measured values for which truncated profile is requested
+     (i.e. charge profile of a track)
+     @input std::vector<float> dq_trunc_v -> passed by reference -> output stored here
+     @input float nsigma -> optional parameter, number of sigma to keep around RMS for TM calculation
+  */
+  void CalcTruncMeanProfile(const std::vector<float>& rr_v, const std::vector<float>& dq_v,
+			    std::vector<float>& dq_trunc_v, const float& nsigma = 1);
+
+  /**
+     @brief Iteratively calculate the truncated mean of a distribution
+     @input std::vector<float> v -> vector of values for which truncated mean is asked
+     @input size_t nmin -> minimum number of iterations to converge on truncated mean
+     @input size_t nmax -> maximum number of iterations to converge on truncated mean
+     @input size_t lmin -> minimum number of entries in vector before exiting and returning current value
+     @input size_t currentiteration -> current iteration
+     @input float convergencelimit -> fractional difference between successive iterations
+     under which the iteration is completed, provided nmin iterations have occurred.
+     @input nsigma -> number of sigma around the median value to keep when the distribution is trimmed.
+   */
+  float CalcIterativeTruncMean(std::vector<float> v, const size_t& nmin,
+			       const size_t& nmax, const size_t& currentiteration,
+			       const size_t& lmin,
+			       const float& convergencelimit,
+			       const float& nsigma, const float& oldmed = kINVALID_FLOAT);
+
+  /**
+     @brief Set the smearing radius over which to take hits for truncated mean computaton.
+   */
+  void setRadius(const float& rad) { _rad = rad; }
+
+ private:
+
+  float Mean  (const std::vector<float>& v);
+  float Median(const std::vector<float>& v);
+  float RMS   (const std::vector<float>& v);
+
+  /**
+     Smearing radius over which charge from neighboring hits is scanned to calculate local
+     truncated mean
+   */
+  double _rad;
+
+};
+
+float TruncMean::CalcIterativeTruncMean(std::vector<float> v, const size_t& nmin,
+					const size_t& nmax, const size_t& currentiteration,
+					const size_t& lmin,
+					const float& convergencelimit,
+					const float& nsigma, const float& oldmed)
+{
+
+  auto const& mean = Mean(v);
+  auto const& med  = Median(v);
+  auto const& rms  = RMS(v);
+
+  // if the vector length is below the lower limit -> return
+  if (v.size() < lmin)
+    return mean;
+
+  // if we have passed the maximum number of iterations -> return
+  if (currentiteration >= nmax)
+    return mean;
+
+  // if we passed the minimum number of iterations and the mean is close enough to the old value
+  float fracdiff = fabs(med-oldmed) / oldmed;
+  if ( (currentiteration >= nmin) && (fracdiff < convergencelimit) )
+    return mean;
+  
+  // if reached here it means we have to go on for another iteration
+
+  // cutoff tails of distribution surrounding the mean
+  // use erase-remove : https://en.wikipedia.org/wiki/Erase%E2%80%93remove_idiom
+  // https://stackoverflow.com/questions/17270837/stdvector-removing-elements-which-fulfill-some-conditions
+  v.erase( std::remove_if( v.begin(), v.end(), 
+			   [med,nsigma,rms](const float& x) { return ( (x < (med-nsigma*rms)) || (x > (med+nsigma*rms)) ); }), // lamdda condition for events to be removed
+	   v.end());
+  
+  return CalcIterativeTruncMean(v, nmin, nmax, lmin, currentiteration+1, convergencelimit, nsigma, med);
+}
+
+void TruncMean::CalcTruncMeanProfile(const std::vector<float>& rr_v, const std::vector<float>& dq_v,
+				     std::vector<float>& dq_trunc_v, const float& nsigma)
+{
+
+  // how many points to sample 
+  int Nneighbor = (int)(_rad * 3 * 2);
+
+  dq_trunc_v.clear();
+  dq_trunc_v.reserve( rr_v.size() );
+
+  int Nmax = dq_v.size()-1;
+  
+  for (size_t n=0; n < dq_v.size(); n++) {
+
+    // current residual range
+    float rr = rr_v.at(n);
+
+    int nmin = n - Nneighbor;
+    int nmax = n + Nneighbor;
+
+    if (nmin < 0) nmin = 0;
+    if (nmax > Nmax) nmax = Nmax;
+
+    // vector for local dq values
+    std::vector<float> dq_local_v;
+
+    for (int i=nmin; i < nmax; i++) {
+      
+      float dr = rr - rr_v[i];
+      if (dr < 0) dr *= -1;
+
+      if (dr > _rad) continue;
+
+      dq_local_v.push_back( dq_v[i] );
+      
+    }// for all ticks we want to scan
+
+    if (dq_local_v.size() == 0) {
+      dq_trunc_v.push_back( dq_v.at(n) );
+      continue;
+    }
+    
+    // calculate median and rms
+    float median = Median(dq_local_v);
+    float rms    = RMS(dq_local_v);
+
+    float truncated_dq = 0.;
+    int npts = 0;
+    for (auto const& dq : dq_local_v) {
+      if ( ( dq < (median+rms * nsigma) ) && ( dq > (median-rms * nsigma) ) ){
+	truncated_dq += dq;
+	npts += 1;
+      }
+    }
+
+    dq_trunc_v.push_back( truncated_dq / npts );
+  }// for all values
+
+  return;
+}
+
+float TruncMean::Mean(const std::vector<float>& v)
+{
+
+  float mean = 0.;
+  for (auto const& n : v) mean += n;
+  mean /= v.size();
+  
+  return mean;
+}
+
+float TruncMean::Median(const std::vector<float>& v)
+{
+
+  if (v.size() == 1) return v[0];
+  
+  std::vector<float> vcpy = v;
+
+  std::sort(vcpy.begin(), vcpy.end());
+
+  float median = vcpy[ vcpy.size() / 2 ];
+
+  return median;
+}
+
+float TruncMean::RMS(const std::vector<float>& v)
+{
+
+  float avg = 0.;
+  for (auto const& val : v) avg += val;
+  avg /= v.size();
+  float rms = 0.;
+  for (auto const& val : v) rms += (val-avg)*(val-avg);
+  rms = sqrt( rms / ( v.size() -  1 ) );
+
+  return rms;
+}
 
 
 class StopMu;
@@ -72,6 +297,8 @@ private:
 
   std::string fTrkProducer, fCaloProducer, fMCProducer, fMCParticleLabel, fOpticalFlashFinderLabel;
 
+  TruncMean _tmean;
+
   TTree* _reco_tree;
   float _trk_len;
   float _trk_start_x;
@@ -83,10 +310,13 @@ private:
   float _yz_true_reco_distance;
   float _pitch_u, _pitch_v, _pitch_y;
   std::vector<float> _dqdx_u;
+  std::vector<float> _dqdx_tm_u;
   std::vector<float> _rr_u;
   std::vector<float> _dqdx_v;
+  std::vector<float> _dqdx_tm_v;
   std::vector<float> _rr_v;
   std::vector<float> _dqdx_y;
+  std::vector<float> _dqdx_tm_y;
   std::vector<float> _rr_y;
   float _delta_t_closest_flash;
 };
@@ -102,6 +332,8 @@ StopMu::StopMu(fhicl::ParameterSet const & p)
   fMCProducer = p.get<std::string>("MCProducer", "generator");
   fMCParticleLabel = p.get<std::string>("MCParticleLabel", "largeant");
   fOpticalFlashFinderLabel = p.get<std::string>("OpticalFlashFinderLabel", "simpleFlashCosmic");
+
+  _tmean.setRadius(10);
 
   art::ServiceHandle<art::TFileService> tfs;
 
@@ -133,6 +365,9 @@ StopMu::StopMu(fhicl::ParameterSet const & p)
   _reco_tree->Branch("_dqdx_u","std::vector<float>",&_dqdx_u);
   _reco_tree->Branch("_dqdx_v","std::vector<float>",&_dqdx_v);
   _reco_tree->Branch("_dqdx_y","std::vector<float>",&_dqdx_y);
+  _reco_tree->Branch("_dqdx_tm_u","std::vector<float>",&_dqdx_tm_u);
+  _reco_tree->Branch("_dqdx_tm_v","std::vector<float>",&_dqdx_tm_v);
+  _reco_tree->Branch("_dqdx_tm_y","std::vector<float>",&_dqdx_tm_y);
   _reco_tree->Branch("_rr_u",  "std::vector<float>",&_rr_u  );
   _reco_tree->Branch("_rr_v",  "std::vector<float>",&_rr_v  );
   _reco_tree->Branch("_rr_y",  "std::vector<float>",&_rr_y  );
@@ -247,14 +482,14 @@ void StopMu::analyze(art::Event const & e)
 
     for (size_t f=0; f < optical_handle->size(); f++)
     {
-      std::cout << "iteration beg" << f << " delta t " << _delta_t_closest_flash << std::endl;
+      //std::cout << "iteration beg" << f << " delta t " << _delta_t_closest_flash << std::endl;
       auto const& flash = optical_handle->at(f);
       float ttrk = _trk_end_x / 0.1114 - flash.Time() - 6.0;
       if (fabs(ttrk) < fabs(_delta_t_closest_flash)) _delta_t_closest_flash = ttrk;
       ttrk = (_trk_end_x-256.35) / 0.1114 - flash.Time() - 19.3;
       if (fabs(ttrk) < fabs(_delta_t_closest_flash)) _delta_t_closest_flash = ttrk;
 
-      std::cout << "iteration " << f << " delta t " << _delta_t_closest_flash << " ttrk " << ttrk << std::endl;
+      //std::cout << "iteration " << f << " delta t " << _delta_t_closest_flash << " ttrk " << ttrk << std::endl;
     }
     _reco_tree->Fill();
   }
@@ -269,10 +504,13 @@ float StopMu::yzDistance(float y1, float z1, float y2, float z2)
 void StopMu::clear()
 {
   _dqdx_u.clear();
+  _dqdx_tm_u.clear();
   _rr_u.clear();
   _dqdx_v.clear();
+  _dqdx_tm_v.clear();
   _rr_v.clear();
   _dqdx_y.clear();
+  _dqdx_tm_y.clear();
   _rr_y.clear();
 
   _delta_t_closest_flash = 10000.;
@@ -286,6 +524,7 @@ void StopMu::fillCalorimetry(int pl, std::vector<double> dqdx, std::vector<doubl
     {
     _dqdx_u.push_back((float)dqdx[n]);
     _rr_u.push_back(  (float)rr[n]  );
+    _tmean.CalcTruncMeanProfile(_rr_u,_dqdx_u,_dqdx_tm_u);
     }
   }
   else if (pl==1)
@@ -294,6 +533,7 @@ void StopMu::fillCalorimetry(int pl, std::vector<double> dqdx, std::vector<doubl
     {
     _dqdx_v.push_back((float)dqdx[n]);
     _rr_v.push_back(  (float)rr[n]  );
+    _tmean.CalcTruncMeanProfile(_rr_v,_dqdx_v,_dqdx_tm_v);
     }
   }
   else if (pl==2)
@@ -302,6 +542,7 @@ void StopMu::fillCalorimetry(int pl, std::vector<double> dqdx, std::vector<doubl
     {
     _dqdx_y.push_back((float)dqdx[n]);
     _rr_y.push_back(  (float)rr[n]  );
+    _tmean.CalcTruncMeanProfile(_rr_y,_dqdx_y,_dqdx_tm_y);
     }
   }
 }
