56c77d300ae91248486cac43502b3b16899d03ac
John Marshall <John.Marshall@warwick.ac.uk>
2019-09-04 13:33:50 +0100

    Add option to filter candidate vertices that are put forward for later analysis.

--- ubreco/MicroBooNEPandora/DummyVertexParticleCreationAlgorithm.cc
+++ ubreco/MicroBooNEPandora/DummyVertexParticleCreationAlgorithm.cc
@@ -8,6 +8,10 @@
 
 #include "Pandora/AlgorithmHeaders.h"
 
+#include "larpandoracontent/LArHelpers/LArGeometryHelper.h"
+
+#include "larpandoracontent/LArUtility/KDTreeLinkerAlgoT.h"
+
 #include "DummyVertexParticleCreationAlgorithm.h"
 
 using namespace pandora;
@@ -17,7 +21,13 @@ namespace lar_content
 
 DummyVertexParticleCreationAlgorithm::DummyVertexParticleCreationAlgorithm() :
     m_replaceCurrentVertexList(false),
-    m_replaceCurrentPfoList(false)
+    m_replaceCurrentPfoList(false),
+    m_shouldFilterVertexList(false),
+    m_maxOnHitDisplacement(1.f),
+    m_useDetectorGaps(true),
+    m_gapTolerance(0.f),
+    m_isEmptyViewAcceptable(true),
+    m_minVertexAcceptableViews(3)
 {
 }
 
@@ -25,6 +35,9 @@ DummyVertexParticleCreationAlgorithm::DummyVertexParticleCreationAlgorithm() :
 
 StatusCode DummyVertexParticleCreationAlgorithm::Run()
 {
+    HitKDTree2D kdTreeU, kdTreeV, kdTreeW;
+    this->InitializeKDTrees(kdTreeU, kdTreeV, kdTreeW);
+
     const VertexList *pVertexList(nullptr); std::string temporaryVertexListName;
     PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::CreateTemporaryListAndSetCurrent(*this, pVertexList, temporaryVertexListName));
 
@@ -45,7 +58,18 @@ StatusCode DummyVertexParticleCreationAlgorithm::Run()
         if (STATUS_CODE_SUCCESS != PandoraContentApi::GetList(*this, vertexListName, pInputVertexList))
             continue;
 
-        for (const Vertex *const pInputVertex : *pInputVertexList)
+        VertexVector filteredVertices;
+
+        if (m_shouldFilterVertexList)
+        {
+            this->FilterVertexList(pInputVertexList, kdTreeU, kdTreeV, kdTreeW, filteredVertices);
+        }
+        else
+        {
+            filteredVertices.insert(filteredVertices.end(), pInputVertexList->begin(), pInputVertexList->end());
+        }
+
+        for (const Vertex *const pInputVertex : filteredVertices)
         {
             PandoraContentApi::Vertex::Parameters vertexParameters;
             vertexParameters.m_position = pInputVertex->GetPosition();
@@ -82,6 +106,103 @@ StatusCode DummyVertexParticleCreationAlgorithm::Run()
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
+void DummyVertexParticleCreationAlgorithm::InitializeKDTrees(HitKDTree2D &kdTreeU, HitKDTree2D &kdTreeV, HitKDTree2D &kdTreeW) const
+{
+    for (const std::string &caloHitListName : m_inputCaloHitListNames)
+    {
+        const CaloHitList *pCaloHitList = NULL;
+        PANDORA_THROW_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_INITIALIZED, !=, PandoraContentApi::GetList(*this, caloHitListName, pCaloHitList));
+
+        if (!pCaloHitList || pCaloHitList->empty())
+        {
+            if (PandoraContentApi::GetSettings(*this)->ShouldDisplayAlgorithmInfo())
+                std::cout << "DummyVertexParticleCreationAlgorithm: unable to find calo hit list " << caloHitListName << std::endl;
+
+            continue;
+        }
+
+        const HitType hitType((*(pCaloHitList->begin()))->GetHitType());
+
+        if ((TPC_VIEW_U != hitType) && (TPC_VIEW_V != hitType) && (TPC_VIEW_W != hitType))
+            throw StatusCodeException(STATUS_CODE_INVALID_PARAMETER);
+
+        HitKDTree2D &kdTree((TPC_VIEW_U == hitType) ? kdTreeU : (TPC_VIEW_V == hitType) ? kdTreeV : kdTreeW);
+
+        if (!kdTree.empty())
+            throw StatusCodeException(STATUS_CODE_FAILURE);
+
+        HitKDNode2DList hitKDNode2DList;
+        KDTreeBox hitsBoundingRegion2D(fill_and_bound_2d_kd_tree(*pCaloHitList, hitKDNode2DList));
+        kdTree.build(hitKDNode2DList, hitsBoundingRegion2D);
+    }
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void DummyVertexParticleCreationAlgorithm::FilterVertexList(const VertexList *const pInputVertexList, HitKDTree2D &kdTreeU, HitKDTree2D &kdTreeV,
+    HitKDTree2D &kdTreeW, VertexVector &filteredVertices) const
+{
+    for (const Vertex *const pVertex : *pInputVertexList)
+    {
+        unsigned int nAcceptableViews(0);
+
+        if ((m_isEmptyViewAcceptable && kdTreeU.empty()) || this->IsVertexOnHit(pVertex, TPC_VIEW_U, kdTreeU) || this->IsVertexInGap(pVertex, TPC_VIEW_U))
+            ++nAcceptableViews;
+
+        if ((m_isEmptyViewAcceptable && kdTreeV.empty()) || this->IsVertexOnHit(pVertex, TPC_VIEW_V, kdTreeV) || this->IsVertexInGap(pVertex, TPC_VIEW_V))
+            ++nAcceptableViews;
+
+        if ((m_isEmptyViewAcceptable && kdTreeW.empty()) || this->IsVertexOnHit(pVertex, TPC_VIEW_W, kdTreeW) || this->IsVertexInGap(pVertex, TPC_VIEW_W))
+            ++nAcceptableViews;
+
+        if (nAcceptableViews >= m_minVertexAcceptableViews)
+            filteredVertices.push_back(pVertex);
+    }
+
+    std::sort(filteredVertices.begin(), filteredVertices.end(), SortByVertexZPosition);
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+bool DummyVertexParticleCreationAlgorithm::IsVertexOnHit(const Vertex *const pVertex, const HitType hitType, HitKDTree2D &kdTree) const
+{
+    const CartesianVector vertexPosition2D(LArGeometryHelper::ProjectPosition(this->GetPandora(), pVertex->GetPosition(), hitType));
+    KDTreeBox searchRegionHits = build_2d_kd_search_region(vertexPosition2D, m_maxOnHitDisplacement, m_maxOnHitDisplacement);
+
+    HitKDNode2DList found;
+    kdTree.search(searchRegionHits, found);
+
+    return (!found.empty());
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+bool DummyVertexParticleCreationAlgorithm::IsVertexInGap(const Vertex *const pVertex, const HitType hitType) const
+{
+    if (!m_useDetectorGaps)
+        return false;
+
+    return LArGeometryHelper::IsInGap3D(this->GetPandora(), pVertex->GetPosition(), hitType, m_gapTolerance);
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+bool DummyVertexParticleCreationAlgorithm::SortByVertexZPosition(const pandora::Vertex *const pLhs, const pandora::Vertex *const pRhs)
+{
+    const CartesianVector deltaPosition(pRhs->GetPosition() - pLhs->GetPosition());
+
+    if (std::fabs(deltaPosition.GetZ()) > std::numeric_limits<float>::epsilon())
+        return (deltaPosition.GetZ() > std::numeric_limits<float>::epsilon());
+
+    if (std::fabs(deltaPosition.GetX()) > std::numeric_limits<float>::epsilon())
+        return (deltaPosition.GetX() > std::numeric_limits<float>::epsilon());
+
+    // ATTN No way to distinguish between vertices if still have a tie in y coordinate
+    return (deltaPosition.GetY() > std::numeric_limits<float>::epsilon());
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
 StatusCode DummyVertexParticleCreationAlgorithm::ReadSettings(const TiXmlHandle xmlHandle)
 {
     PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadVectorOfValues(xmlHandle,
@@ -98,6 +219,30 @@ StatusCode DummyVertexParticleCreationAlgorithm::ReadSettings(const TiXmlHandle 
 
     PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle,
         "ReplaceCurrentPfoList", m_replaceCurrentPfoList));
+
+    PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle,
+        "ShouldFilterVertexList", m_shouldFilterVertexList));
+
+    if (m_shouldFilterVertexList)
+    {
+        PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadVectorOfValues(xmlHandle,
+            "InputCaloHitListNames", m_inputCaloHitListNames));
+
+        PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle,
+            "MaxOnHitDisplacement", m_maxOnHitDisplacement));
+
+        PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle,
+            "UseDetectorGaps", m_useDetectorGaps));
+
+        PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle,
+            "GapTolerance", m_gapTolerance));
+
+        PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle,
+            "IsEmptyViewAcceptable", m_isEmptyViewAcceptable));
+
+        PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle,
+            "MinVertexAcceptableViews", m_minVertexAcceptableViews));
+    }
 
     return STATUS_CODE_SUCCESS;
 }
--- ubreco/MicroBooNEPandora/DummyVertexParticleCreationAlgorithm.h
+++ ubreco/MicroBooNEPandora/DummyVertexParticleCreationAlgorithm.h
@@ -13,6 +13,11 @@
 namespace lar_content
 {
 
+template<typename, unsigned int> class KDTreeLinkerAlgo;
+template<typename, unsigned int> class KDTreeNodeInfoT;
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
 /**
  *  @brief  DummyVertexParticleCreationAlgorithm class
  */
@@ -28,6 +33,62 @@ private:
     pandora::StatusCode Run();
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
+    typedef KDTreeNodeInfoT<const pandora::CaloHit*, 2> HitKDNode2D;
+    typedef std::vector<HitKDNode2D> HitKDNode2DList;
+    typedef KDTreeLinkerAlgo<const pandora::CaloHit*, 2> HitKDTree2D;
+
+    /**
+     *  @brief  Filter the input list of vertices to obtain a reduced number of vertex candidates
+     *
+     *  @param  pInputVertexList the address of the input vertex list
+     *  @param  kdTreeU the kd tree for u hits
+     *  @param  kdTreeV the kd tree for v hits
+     *  @param  kdTreeW the kd tree for w hits
+     *  @param  filteredVertices to receive the filtered vertex list
+     */
+    virtual void FilterVertexList(const pandora::VertexList *const pInputVertexList, HitKDTree2D &kdTreeU, HitKDTree2D &kdTreeV,
+        HitKDTree2D &kdTreeW, pandora::VertexVector &filteredVertices) const;
+
+    /**
+     *  @brief  Initialize kd trees with details of hits in algorithm-configured cluster lists
+     *
+     *  @param  kdTreeU the kd tree for u hits
+     *  @param  kdTreeV the kd tree for v hits
+     *  @param  kdTreeW the kd tree for w hits
+     */
+    void InitializeKDTrees(HitKDTree2D &kdTreeU, HitKDTree2D &kdTreeV, HitKDTree2D &kdTreeW) const;
+
+    /**
+     *  @brief  Whether the vertex lies on a hit in the specified view
+     *
+     *  @param  pVertex the address of the vertex
+     *  @param  hitType the relevant hit type
+     *  @param  kdTree the relevant kd tree
+     *
+     *  @return boolean
+     */
+    bool IsVertexOnHit(const pandora::Vertex *const pVertex, const pandora::HitType hitType, HitKDTree2D &kdTree) const;
+
+    /**
+     *  @brief  Whether the vertex lies in a registered gap
+     *
+     *  @param  pVertex the address of the vertex
+     *  @param  hitType the relevant hit type
+     *
+     *  @return boolean
+     */
+    bool IsVertexInGap(const pandora::Vertex *const pVertex, const pandora::HitType hitType) const;
+
+    /**
+     *  @brief  Sort vertices by increasing z position
+     *
+     *  @param  pLhs address of the lhs vertex
+     *  @param  pRhs address of the rhs vertex
+     *
+     *  @return whether lhs should precedes rhs
+     */
+    static bool SortByVertexZPosition(const pandora::Vertex *const pLhs, const pandora::Vertex *const pRhs);
+
     pandora::StringVector   m_inputVertexListNames;      ///< The input vector of vertex list names
 
     std::string             m_outputVertexListName;      ///< The name under which to save the output vertex list
@@ -35,6 +96,17 @@ private:
 
     bool                    m_replaceCurrentVertexList;  ///< Whether to replace the current vertex list with the output list
     bool                    m_replaceCurrentPfoList;     ///< Whether to replace the current pfo list with the output list
+
+    bool                    m_shouldFilterVertexList;    ///< Whether to filter candidate vertices to include only those that sit on/near a hit or in a gap
+    pandora::StringVector   m_inputCaloHitListNames;        ///< The list of calo hit list names
+
+    float                   m_maxOnHitDisplacement;      ///< Max hit-vertex displacement for declaring vertex to lie on a hit in each view
+
+    bool                    m_useDetectorGaps;           ///< Whether to account for registered detector gaps in vertex selection
+    float                   m_gapTolerance;              ///< The tolerance to use when querying whether a sampling point is in a gap, units cm
+
+    bool                    m_isEmptyViewAcceptable;     ///< Whether views entirely empty of hits are classed as 'acceptable' for candidate filtration
+    unsigned int            m_minVertexAcceptableViews;  ///< The minimum number of views in which a candidate must sit on/near a hit or in a gap (or view can be empty)
 };
 
 } // namespace lar_content
