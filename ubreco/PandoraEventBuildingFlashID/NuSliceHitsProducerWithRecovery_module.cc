1cde2105cde60d092dc5af05dbf7d790a6b7e7aa
Giuseppe Cerati <cerati@fnal.gov>
2024-11-22 12:47:19 -0600

    use allOutcomes also for largest score slice recovery

--- ubreco/PandoraEventBuildingFlashID/NuSliceHitsProducerWithRecovery_module.cc
+++ ubreco/PandoraEventBuildingFlashID/NuSliceHitsProducerWithRecovery_module.cc
@@ -126,9 +126,9 @@ void NuSliceHitsProducerWithRecovery::produce(art::Event& e)
   }
 
   //select the neutrino slice, or the one with largest NuScore if there is no neutino slice
-  size_t primaryIdx = inputPfp->size();
+  //size_t sliceIdx = inputSlice->size();
+  std::vector< art::Ptr<recob::Hit> > slice_hit_ptr_v;
   size_t primarySelf = inputPfp->size();
-  float maxNuScore = std::numeric_limits<float>::lowest();
   bool foundNuSlice = false;
   recob::Vertex::Point_t nuvtx;
   for (size_t ipfp = 0; ipfp < inputPfp->size(); ipfp++) {
@@ -148,112 +148,117 @@ void NuSliceHitsProducerWithRecovery::produce(art::Event& e)
 	}
       }
       //
-      primaryIdx = ipfp;
       primarySelf = pfp->Self();
       foundNuSlice = true;
       nuvtx = assocPfpVertex->at(pfp.key()).at(0)->position();
+      slice_hit_ptr_v = assocSliceHit->at(assocPfpSlice->at(pfp.key())[0].key());
       break;
     }
-    //
-    if (fRecoverHighestNuScoreSlice==false) continue;
+  }
 
-    if (!pfParticlePropertiesMap.empty()) {
-      auto it = pfParticlePropertiesMap.begin();
-      while (it != pfParticlePropertiesMap.end()) {
-	//std::cout << "ipfp=" << ipfp << " primary=" << pfp->IsPrimary() << " pdg=" << pfp->PdgCode() << " meta=" << it->first << " " << it->second << std::endl;
-	if (it->first == "NuScore") {
-	  std::cout << "ipfp=" << ipfp << " self=" << pfp->Self() << " primary=" << pfp->IsPrimary() << " pdg=" << pfp->PdgCode() << " meta=" << it->first << " " << it->second << std::endl;
+  //
+  //get the slice with largest NuScore, from the "allOutcomes" output so that we can look at it under the neutrino interpretation
+  auto const& slice_ao_h = e.getValidHandle<std::vector<recob::Slice> >(fAllOutcomesLabel);
+  auto const& pfp_ao_h = e.getValidHandle<std::vector<recob::PFParticle> >(fAllOutcomesLabel);
+  art::FindOneP<larpandoraobj::PFParticleMetadata> assocPfpMetadata_AO(pfp_ao_h, e, fAllOutcomesLabel);
+  art::FindManyP<recob::PFParticle> assocSlicePfp_AO(slice_ao_h, e, fAllOutcomesLabel);
+  art::FindManyP<recob::Vertex> pfp_vertex_ao_assn_v(pfp_ao_h, e, fAllOutcomesLabel);
+  art::FindManyP<recob::Hit> assocSliceHit_AO(slice_ao_h, e, fAllOutcomesLabel);
+  //
+  //std::cout << "assocSlicePfp.size()=" << assocSlicePfp_AO.size() << std::endl;
+  std::vector<art::Ptr<recob::Slice> > slice_ao_ptr_v;
+  std::vector<art::Ptr<recob::PFParticle> > pfp_ao_ptr_v;
+  art::fill_ptr_vector(slice_ao_ptr_v,slice_ao_h);
+  art::fill_ptr_vector(pfp_ao_ptr_v,pfp_ao_h);
+  std::map<int, art::Ptr<recob::PFParticle>> pfpmap_ao;
+  lar_pandora::LArPandoraHelper::BuildPFParticleMap(pfp_ao_ptr_v,pfpmap_ao);
+  //
+  if (foundNuSlice==false && fRecoverHighestNuScoreSlice) {
+    //
+    float maxNuScore = std::numeric_limits<float>::lowest();
+    for (auto& slice : slice_ao_ptr_v) {
+      const std::vector<art::Ptr<recob::PFParticle> >& nuSlicePFPs_AO(assocSlicePfp_AO.at(slice.key()));
+      for (auto& pfp_AO : nuSlicePFPs_AO) {
+	auto pfp_AO_parent = lar_pandora::LArPandoraHelper::GetParentPFParticle(pfpmap_ao,pfp_AO);
+	if (!lar_pandora::LArPandoraHelper::IsNeutrino(pfp_AO_parent) ) continue;
+	if (pfp_AO->IsPrimary()==false || pfp_AO_parent->Self()==primarySelf) continue;
+	//
+	auto pfParticleMetadata = assocPfpMetadata_AO.at(pfp_AO.key());
+	auto pfParticlePropertiesMap = pfParticleMetadata->GetPropertiesMap();
+	//
+	for (auto it = pfParticlePropertiesMap.begin(); it != pfParticlePropertiesMap.end(); it++)  {
+	  //std::cout << "ipfp=" << ipfp << " primary=" << pfp->IsPrimary() << " pdg=" << pfp->PdgCode() << " meta=" << it->first << " " << it->second << std::endl;
+	  if (it->first != "NuScore") continue;
+	  std::cout << "pfp  self=" << pfp_AO->Self() << " primary=" << pfp_AO->IsPrimary() << " pdg=" << pfp_AO->PdgCode() << " meta=" << it->first << " " << it->second << std::endl;
 	  if (pfParticlePropertiesMap.at(it->first)>maxNuScore) {
-	    primaryIdx = ipfp;
-	    primarySelf = pfp->Self();
+	    primarySelf = pfp_AO->Self();
 	    maxNuScore = pfParticlePropertiesMap.at(it->first);
-	    nuvtx = assocPfpVertex->at(pfp.key()).at(0)->position();//fixme: this is not the "neutrino" vertex since this is a cosmic slice
+	    nuvtx = pfp_vertex_ao_assn_v.at(pfp_AO.key()).at(0)->position();
+	    slice_hit_ptr_v = assocSliceHit_AO.at(slice.key());
 	  }
 	}
-	it++;
       }
-    } // if PFP metadata exists!
+    }
   }
 
   //now save the hits for this slice
-  if (primaryIdx < inputPfp->size()) {
-    art::Ptr<recob::PFParticle> pfp(inputPfp, primaryIdx);
-
-    auto assocSlice = assocPfpSlice->at(pfp.key());
-    auto sliceHits = assocSliceHit->at(assocSlice[0].key());
-
-    for (size_t ihit = 0; ihit < sliceHits.size(); ++ihit) {
-      auto hit = sliceHits.at(ihit);
-      outputHits->emplace_back(*hit);
-
-      if (!hittruth) continue;
-      std::vector<art::Ptr<simb::MCParticle>> particle_vec = hittruth->at(hit.key());
-      if (particle_vec.size()>0) nAssocHits++;
-      std::vector<anab::BackTrackerHitMatchingData const*> match_vec = hittruth->data(hit.key());
-      const art::Ptr<recob::Hit> ahp = hitPtrMaker(outputHits->size() - 1);
-      for (size_t i_p = 0; i_p < particle_vec.size(); ++i_p) {
-        outputHitPartAssns->addSingle(particle_vec[i_p], ahp, *match_vec[i_p]);
-      }
+  for (size_t ihit = 0; ihit < slice_hit_ptr_v.size(); ++ihit) {
+    auto hit = slice_hit_ptr_v.at(ihit);
+    outputHits->emplace_back(*hit);
+    //
+    if (!hittruth) continue;
+    std::vector<art::Ptr<simb::MCParticle>> particle_vec = hittruth->at(hit.key());
+    if (particle_vec.size()>0) nAssocHits++;
+    std::vector<anab::BackTrackerHitMatchingData const*> match_vec = hittruth->data(hit.key());
+    const art::Ptr<recob::Hit> ahp = hitPtrMaker(outputHits->size() - 1);
+    for (size_t i_p = 0; i_p < particle_vec.size(); ++i_p) {
+      outputHitPartAssns->addSingle(particle_vec[i_p], ahp, *match_vec[i_p]);
     }
+  }
 
-    //now try to recover hits from potential 2nd showers in other slices
-    if (fRecover2ndShower) {
-      //
-      //get the slice with largest NuScore, from the "allOutcomes" output so that we can look at it under the neutrino interpretation
-      auto const& slice_ao_h = e.getValidHandle<std::vector<recob::Slice> >(fAllOutcomesLabel);
-      auto const& pfp_ao_h = e.getValidHandle<std::vector<recob::PFParticle> >(fAllOutcomesLabel);
-      auto const& cluster_ao_h = e.getValidHandle<std::vector<recob::Cluster> >(fAllOutcomesLabel);
-      art::FindOneP<larpandoraobj::PFParticleMetadata> assocPfpMetadata_AO(pfp_ao_h, e, fAllOutcomesLabel);
-      art::FindManyP<recob::PFParticle> assocSlicePfp_AO(slice_ao_h, e, fAllOutcomesLabel);
-      art::FindManyP<recob::Cluster> pfp_cluster_ao_assn_v(pfp_ao_h, e, fAllOutcomesLabel);
-      art::FindManyP<recob::Vertex> pfp_vertex_ao_assn_v(pfp_ao_h, e, fAllOutcomesLabel);
-      art::FindManyP<recob::Hit> cluster_hit_ao_assn_v(cluster_ao_h, e, fAllOutcomesLabel);
-      //
-      //std::cout << "assocSlicePfp.size()=" << assocSlicePfp_AO.size() << std::endl;
-      std::vector<art::Ptr<recob::Slice> > slice_ao_ptr_v;
-      std::vector<art::Ptr<recob::PFParticle> > pfp_ao_ptr_v;
-      art::fill_ptr_vector(slice_ao_ptr_v,slice_ao_h);
-      art::fill_ptr_vector(pfp_ao_ptr_v,pfp_ao_h);
-      std::map<int, art::Ptr<recob::PFParticle>> pfpmap_ao;
-      lar_pandora::LArPandoraHelper::BuildPFParticleMap(pfp_ao_ptr_v,pfpmap_ao);
-      //
-      for (auto& slice : slice_ao_ptr_v) {
-	const std::vector<art::Ptr<recob::PFParticle> >& nuSlicePFPs_AO(assocSlicePfp_AO.at(slice.key()));
-	for (auto& pfp_AO : nuSlicePFPs_AO) {
-	  auto pfp_AO_parent = lar_pandora::LArPandoraHelper::GetParentPFParticle(pfpmap_ao,pfp_AO);
-	  if (!lar_pandora::LArPandoraHelper::IsNeutrino(pfp_AO_parent) ) continue;
-	  if (pfp_AO->IsPrimary() || pfp_AO_parent->Self()==primarySelf) continue;
-	  //std::cout << "self=" << pfp_AO->Self() << " " << " parent=" << pfp_AO->Parent() << " primary=" << primarySelf << " parent2=" << lar_pandora::LArPandoraHelper::GetParentPFParticle(pfpmap_ao,pfp_AO)->Self() << std::endl;
-	  // only pfps within fVtxDistCut of the neutrino vertex
-	  if (pfp_vertex_ao_assn_v.at(pfp_AO.key()).size()==0) continue;
-	  auto pfvtx = pfp_vertex_ao_assn_v.at(pfp_AO.key()).at(0)->position();
-	  //std::cout << "pfp vtx dist=" << std::sqrt( (nuvtx-pfvtx).Mag2() ) << std::endl;
-	  if ( (nuvtx-pfvtx).Mag2() > fVtxDistCut2 ) continue;
-	  //
-	  const std::vector< art::Ptr<recob::Cluster> > this_cluster_ptr_v = pfp_cluster_ao_assn_v.at( pfp_AO.key() );
-	  //
-	  int nhits = 0;
-	  for (auto cluster_ptr : this_cluster_ptr_v) nhits += cluster_hit_ao_assn_v.at( cluster_ptr.key() ).size();
-	  //
-	  //consider only showers, or tracks with a limited number of hits consistent with a pi0 shower
-	  if (pfp_AO->PdgCode()==13 && nhits>fMaxHitCut) continue;
-	  //
-	  std::cout << "out-of-slice recovery: adding hits from this pfp pdg=" << pfp_AO->PdgCode() << " nhits=" << nhits << " vtx dist=" << std::sqrt( (nuvtx-pfvtx).Mag2() ) << std::endl;
-	  for (auto cluster_ptr : this_cluster_ptr_v) {
-	    const std::vector< art::Ptr<recob::Hit> > this_hit_ptr_v = cluster_hit_ao_assn_v.at( cluster_ptr.key() );
-	    std::cout << "plane hits size=" << this_hit_ptr_v.size() << std::endl;
-	    for (auto hit : this_hit_ptr_v) {
-	      //std::cout << "hit plane=" << hit->WireID().Plane << " wire=" << hit->WireID().Wire << " time=" << hit->PeakTime() << std::endl;
-	      outputHits->emplace_back(*hit);
-	      //
-	      if (!hittruth) continue;
-	      std::vector<art::Ptr<simb::MCParticle>> particle_vec = hittruth->at(hit.key());
-	      if (particle_vec.size()>0) nAssocHits++;
-	      std::vector<anab::BackTrackerHitMatchingData const*> match_vec = hittruth->data(hit.key());
-	      const art::Ptr<recob::Hit> ahp = hitPtrMaker(outputHits->size() - 1);
-	      for (size_t i_p = 0; i_p < particle_vec.size(); ++i_p) {
-		outputHitPartAssns->addSingle(particle_vec[i_p], ahp, *match_vec[i_p]);
-	      }
+  //now try to recover hits from potential 2nd showers in other slices
+  if (fRecover2ndShower) {
+    //
+    auto const& cluster_ao_h = e.getValidHandle<std::vector<recob::Cluster> >(fAllOutcomesLabel);
+    art::FindManyP<recob::Cluster> pfp_cluster_ao_assn_v(pfp_ao_h, e, fAllOutcomesLabel);
+    art::FindManyP<recob::Hit> cluster_hit_ao_assn_v(cluster_ao_h, e, fAllOutcomesLabel);
+    //
+    for (auto& slice : slice_ao_ptr_v) {
+      const std::vector<art::Ptr<recob::PFParticle> >& nuSlicePFPs_AO(assocSlicePfp_AO.at(slice.key()));
+      for (auto& pfp_AO : nuSlicePFPs_AO) {
+	auto pfp_AO_parent = lar_pandora::LArPandoraHelper::GetParentPFParticle(pfpmap_ao,pfp_AO);
+	if (!lar_pandora::LArPandoraHelper::IsNeutrino(pfp_AO_parent) ) continue;
+	if (pfp_AO->IsPrimary() || pfp_AO_parent->Self()==primarySelf) continue;
+	std::cout << "self=" << pfp_AO->Self() << " " << " parent=" << pfp_AO->Parent() << " primary=" << primarySelf << " parent2=" << lar_pandora::LArPandoraHelper::GetParentPFParticle(pfpmap_ao,pfp_AO)->Self() << std::endl;
+	// only pfps within fVtxDistCut of the neutrino vertex
+	if (pfp_vertex_ao_assn_v.at(pfp_AO.key()).size()==0) continue;
+	auto pfvtx = pfp_vertex_ao_assn_v.at(pfp_AO.key()).at(0)->position();
+	//std::cout << "pfp vtx dist=" << std::sqrt( (nuvtx-pfvtx).Mag2() ) << std::endl;
+	if ( (nuvtx-pfvtx).Mag2() > fVtxDistCut2 ) continue;
+	//
+	const std::vector< art::Ptr<recob::Cluster> > this_cluster_ptr_v = pfp_cluster_ao_assn_v.at( pfp_AO.key() );
+	//
+	int nhits = 0;
+	for (auto cluster_ptr : this_cluster_ptr_v) nhits += cluster_hit_ao_assn_v.at( cluster_ptr.key() ).size();
+	//
+	//consider only showers, or tracks with a limited number of hits consistent with a pi0 shower
+	if (pfp_AO->PdgCode()==13 && nhits>fMaxHitCut) continue;
+	//
+	std::cout << "out-of-slice recovery: adding hits from this pfp pdg=" << pfp_AO->PdgCode() << " nhits=" << nhits << " vtx dist=" << std::sqrt( (nuvtx-pfvtx).Mag2() ) << std::endl;
+	for (auto cluster_ptr : this_cluster_ptr_v) {
+	  const std::vector< art::Ptr<recob::Hit> > this_hit_ptr_v = cluster_hit_ao_assn_v.at( cluster_ptr.key() );
+	  std::cout << "plane hits size=" << this_hit_ptr_v.size() << std::endl;
+	  for (auto hit : this_hit_ptr_v) {
+	    //std::cout << "hit plane=" << hit->WireID().Plane << " wire=" << hit->WireID().Wire << " time=" << hit->PeakTime() << std::endl;
+	    outputHits->emplace_back(*hit);
+	    //
+	    if (!hittruth) continue;
+	    std::vector<art::Ptr<simb::MCParticle>> particle_vec = hittruth->at(hit.key());
+	    if (particle_vec.size()>0) nAssocHits++;
+	    std::vector<anab::BackTrackerHitMatchingData const*> match_vec = hittruth->data(hit.key());
+	    const art::Ptr<recob::Hit> ahp = hitPtrMaker(outputHits->size() - 1);
+	    for (size_t i_p = 0; i_p < particle_vec.size(); ++i_p) {
+	      outputHitPartAssns->addSingle(particle_vec[i_p], ahp, *match_vec[i_p]);
 	    }
 	  }
 	}
